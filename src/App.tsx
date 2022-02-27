8cb5de847e7aa397a19916a99304857629431f2f
Eniken-Benaken <strippedstar@gmail.com>
2022-02-27 16:17:54 +0700

    started view

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<header className="header">
+				<div className="header__img">React.Net</div>
+			</header>
+			<main>
+				{/* <div className="banner">
+					<img src="https://source.unsplash.com/900x300/?beach" alt="banner" />
+				</div> */}
+				<div className="profile">
+					<div className="profile__photo">
+					</div>
+					<div className="profile__info">
+						<h3 className="profile__name">Dmitry Golub</h3>
+						<p className="profile__birthdate">Date of Birth: 13 february</p>
+						<p className="profile__city">City: Sevastopol</p>
+						<p className="profile__education">Education: SCPTE</p>
+						<p className="profile__website">Web Site: <a href="https://github.com/eniken-benaken">https://github.com/eniken-benaken</a></p>
+					</div>
+				</div>
+				<div className="posts">
+					<h3>My posts</h3>
+					<div className="post__new">
+						<form action="#">
+							<textarea name="post__new" id="post__new" cols={120} placeholder='Tell everyone about what you feel...'></textarea>
+							<button type="submit">Send</button>
+						</form>
+					</div>
+					<div className="post" id={`post2`}>
+						<div className="post__avatar">
+							<img src="https://source.unsplash.com/50x50/?people" alt="post__avatar" />
+						</div>
+						<div className="post__message">Is anyone here?</div>
+					</div>
+					<div className="post" id={`post1`}>
+						<div className="post__avatar">
+							<img src="https://source.unsplash.com/50x50/?people" alt="post__avatar" />
+						</div>
+						<div className="post__message">Hey everybody! This is my first post</div>
+					</div>
+				</div>
+			</main>
+			<aside className="navbar">
+				<div className="navbar__item">Profile</div>
+				<div className="navbar__item">Messages</div>
+				<div className="navbar__item">News</div>
+				<div className="navbar__item">Music</div>
+				<div className="navbar__item">Settings</div>
+			</aside>
+		</div>
+	);
 }
 
 export default App;
